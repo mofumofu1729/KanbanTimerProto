539e1722b7f05b5f22775434ee58621f043ca507
mofumofuchan <you_oki1@yahoo.co.jp>
2021-05-20 22:21:18 +0900

    カラムを増やす

--- src/initial-data.js
+++ src/initial-data.js
@@ -13,17 +13,32 @@ const initialData = {
     },
     'column-2': {
       id: 'column-2',
-      title: 'In progress',
+      title: 'This week',
       taskIds: [],
     },
     'column-3': {
       id: 'column-3',
+      title: 'Today',
+      taskIds: [],
+    },
+    'column-4': {
+      id: 'column-4',
+      title: 'In Progress',
+      taskIds: [],
+    },
+    'column-5': {
+      id: 'column-5',
+      title: 'Pending/Calculating',
+      taskIds: [],
+    },
+    'column-6': {
+      id: 'column-6',
       title: 'Done',
       taskIds: [],
     },
 
   },
-  columnOrder: ['column-1', 'column-2', 'column-3'],
+  columnOrder: ['column-1', 'column-2', 'column-3', 'column-4', 'column-5', 'column-6'],
   textBox: 'new task',
   timerSecond: 0,
   selectedTask: null,

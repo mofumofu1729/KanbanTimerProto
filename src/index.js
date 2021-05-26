f1f99d3260d0ebf8ca1cf4ba6d89f7edff906bbf
mofumofuchan <you_oki1@yahoo.co.jp>
2021-05-27 00:54:50 +0900

    選択したタスクがテキストボックスに入るよう修正

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     const newState = {
       ...this.state,
       selectedTask: taskId,
+      textBox: this.state.tasks[taskId].content,
     };
     this.setState(newState);
   };
